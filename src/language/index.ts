291eab48a83a429cf8c128f0795d5ba5495257a0
Han <2414221668@qq.com>
2022-01-26 22:30:52 +0800

    优化

--- src/language/index.ts
+++ src/language/index.ts
@@ -4,13 +4,18 @@ import ZH from './zh'
 import EN from './en'
 // 识别浏览器默认语言
 const browserlanguage = (navigator.language || navigator.browserLanguage)
-const language = browserlanguage.slice(0, 2)
+let language = browserlanguage.slice(0, 2)
+if (language == 'zh' || language == 'en') {
+    language = language
+} else {
+    language = 'en'
+}
 console.log(language, 'language')
 // localStorage.setItem('lan', language)
 //注册i8n实例并引入语言文件
 const i18n = createI18n({
     silentTranslationWarn: true,
-    locale: localStorage.getItem('lan') || language,	//默认显示的语言 
+    locale: localStorage.getItem('lan') || language  || 'en',	//默认显示的语言 
     messages: {
         'zh': ZH,
         'en': EN
